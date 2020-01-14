cea287f535eff862c861792c77584d3c53e4cb2c
Umesh2127 <gaikwadumesh123@gmail.com>
2020-01-14 19:27:09 +0530

    Update __init__.py

--- nbschedule/worker/__init__.py
+++ nbschedule/worker/__init__.py
@@ -6,17 +6,17 @@ from ._nbconvert import run as run_nbconvert  # noqa: F401
 from ._papermill import run as run_papermill  # noqa: F401
 
 def run(job, reports, working_dir=None):
-    logging.critical('Calling run on job - %s' % str(job))
+    logging.critical('Calling run on job - %s' % job["id"])
     for rep in reports:
         # type is "convert" for nbconversion jobs and "publish" for publish jobs
-        type = rep.meta.type
+        type = rep["meta"]["type"]
 
         # Papermill task, performs the report creation
         # using papermill and the report's individual
         # parameters and configuration
-        papermilled = run_papermill(rep.meta.notebook.name,
-                                    rep.meta.notebook.meta.notebook,
-                                    rep.meta.parameters,
-                                    rep.meta.strip_code,
-                                    rep.meta.output_path,
-                                    rep.id)
+        papermilled = run_papermill(rep['meta']['notebook'],
+                                    rep['meta']['notebook_text'],
+                                    rep['meta']['parameters'],
+                                    rep['meta']['strip_code'],
+                                    rep['meta']['output_path'],
+                                    rep['id'])
