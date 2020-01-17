2b9faccd984ee7ad82a8f6818a987472bf3d486b
Umesh2127 <gaikwadumesh123@gmail.com>
2020-01-17 18:13:26 +0530

    Update __init__.py

--- nbschedule/worker/__init__.py
+++ nbschedule/worker/__init__.py
@@ -6,7 +6,7 @@ from ._nbconvert import run as run_nbconvert  # noqa: F401
 from ._papermill import run as run_papermill  # noqa: F401
 
 def run(notebook_name, notebook_text, parameters_list, output_path, working_dir=None):
-    logging.critical('Calling run on job - %s' % job["id"])
+    logging.critical('Calling run on job - %s' % notebook_name)
     for i, parameters in enumerate(parameters_list, start=1):
         # type is "convert" for nbconversion jobs and "publish" for publish jobs
         # type = rep["meta"]["type"]
