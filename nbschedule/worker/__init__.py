ee6a2cb1ebe588230a69599f8df9ade0ccac91d3
Umesh2127 <gaikwadumesh123@gmail.com>
2020-01-18 11:12:01 +0530

    Updated __init__.py in worker
    run methon will take job_json argument of type byte (i.e. b64encode)

--- nbschedule/worker/__init__.py
+++ nbschedule/worker/__init__.py
@@ -1,16 +1,24 @@
+import json
 import os
 import os.path
 import logging
 from nbschedule.utils import name_to_class
 from ._nbconvert import run as run_nbconvert  # noqa: F401
 from ._papermill import run as run_papermill  # noqa: F401
+from base64 import b64decode
 
-def run(notebook_name, notebook_text, parameters_list, output_path, working_dir=None):
+def run(job_json, working_dir=None):
+
+    parsed_json = json.loads(b64decode(job_json).decode('utf-8'))
+
+    notebook_name = parsed_json['name']
+    notebook_text = parsed_json['notebook_text']
+    parameters_list = parsed_json['parameters_list']
+    output_path = parsed_json['output_path']
     logging.critical('Calling run on job - %s' % notebook_name)
     for i, parameters in enumerate(parameters_list, start=1):
         # type is "convert" for nbconversion jobs and "publish" for publish jobs
         # type = rep["meta"]["type"]
-
         # Papermill task, performs the report creation
         # using papermill and the report's individual
         # parameters and configuration
