a0b9f03f722b8b359457fa6cef396499068f317d
Ashutosh <ashutosh0710@gmail.com>
2020-02-12 18:17:19 +0530

    added support for loading spark magic extension, and converted bytes to string

--- nbschedule/worker/_remotemagic.py
+++ nbschedule/worker/_remotemagic.py
@@ -6,8 +6,8 @@ import json
 
 def get_execution_conf(execution_details,session_name):
     encoded_conf = base64.b64encode(json.dumps(execution_details).encode())
-    ext_conf = "%spark encoded -s {} -l {} -f {}".format(session_name,"python",encoded_conf)
-    conf = "{\"execution_count\": null, \"cell_type\": \"code\", \"source\": [\"%s\"], \"outputs\": [], \"metadata\": {}}"%(ext_conf)
+    ext_conf = "%spark encoded -s {} -l {} -f {}".format(session_name,"python",encoded_conf.decode("utf-8"))
+    conf = "{\"execution_count\": null, \"cell_type\": \"code\", \"source\": [\"%s\",\"%s\"], \"outputs\": [], \"metadata\": {}}"%(r"%load_ext sparkmagic.magics\n",ext_conf)
     execution_conf = json.loads(conf)
     return execution_conf
 
@@ -18,5 +18,5 @@ class Remotemagic():
         nb = nbformat.reads(json.dumps(nb_text),4)
         execution_conf = get_execution_conf(execution_details, session_name)
         final = nbformat.from_dict(execution_conf)
-        nb['cells'].append(final)
+        nb['cells'].insert(0,final)
         return nb
