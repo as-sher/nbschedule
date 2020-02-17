0e092a3214695c6ea181a8fd3755ea7b6c077b3a
Umesh2127 <gaikwadumesh123@gmail.com>
2020-02-17 11:47:22 +0530

    added installation of sparkmagic in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup, find_packages
 import os
 import sys
+import pip
+
 
 _here = os.path.abspath(os.path.dirname(__file__))
 
 version = {}
 with open(os.path.join(_here, 'nbschedule', 'version.py')) as f:
     exec(f.read(), version)
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+install("git+https://github.com/as-sher/sparkmagic.git#subdirectory=hdijupyterutils")
+install("git+https://github.com/as-sher/sparkmagic.git#subdirectory=autovizwidget")
+install("git+https://github.com/as-sher/sparkmagic.git#subdirectory=sparkmagic")
+
 
 setup(
     name='nbschedule',
@@ -16,9 +29,11 @@ setup(
     license='MPL-2.0',
     packages=find_packages(),
 #   no dependencies in this example
-#   install_requires=[
-#       'dependency==1.2.3',
-#   ],
+    install_requires=[
+        "ipykernel==5.1.3",
+    ],
+    dependency_links = [
+    ],
 #   no scripts in this example
 #   scripts=['bin/a-script'],
     include_package_data=True,
