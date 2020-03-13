38c705147d75a95c98f3752cac672878f0f30a8c
Ashutosh <ashutosh0710@gmail.com>
2020-03-13 15:15:12 +0530

    Added requirement.txt for all the required dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,19 @@ version = {}
 with open(os.path.join(_here, 'nbschedule', 'version.py')) as f:
     exec(f.read(), version)
 
+python_2 = sys.version_info[0] == 2
+
+def read(fname):
+    with open(fname, 'rU' if python_2 else 'r') as fhandle:
+        return fhandle.read()
+
+
+def read_reqs(fname):
+    req_path = os.path.join(_here, fname)
+    return [req.strip() for req in read(req_path).splitlines() if req.strip()]
+
+all_reqs = read_reqs('requirements.txt')
+
 setup(
     name='nbschedule',
     version=version['__version__'],
@@ -15,14 +28,7 @@ setup(
     long_description='notebook scheduler',
     license='MPL-2.0',
     packages=find_packages(),
-    #   no dependencies in this example
-    #   install_requires=[
-    #       "git+https://github.com/as-sher/sparkmagic.git#subdirectory=hdijupyterutils",
-    #       "git+https://github.com/as-sher/sparkmagic.git#subdirectory=autovizwidget",
-    #       "git+https://github.com/as-sher/sparkmagic.git#subdirectory=sparkmagic"
-    #   ],
-    #   no scripts in this example
-    #   scripts=['bin/a-script'],
+    install_requires= all_reqs,
     include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
